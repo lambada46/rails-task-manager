16e76c1c59d3a9dfbdcf7e7c12ba1859674b7211
lucas furtado <lambada46@gmail.com>
2017-11-07 12:27:02 -0200

    added time and location to the task model

---
+++ db/migrate/20171107142633_add_time_to_tasks.rb
@@ -0,0 +1,4 @@
+class AddTimeToTasks < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171107130144) do
+ActiveRecord::Schema.define(version: 20171107142633) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "title"
